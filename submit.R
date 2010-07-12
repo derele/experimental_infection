49e37586a5f59fcf8af766fc0a1beb68deab9433
derele <emanuelheitlinger@gmail.com>
2010-07-12 12:49:46 +0200

    The initial data and selection for preparation

---
+++ submit.R
@@ -0,0 +1,7 @@
+# This file is a indirect result of eeldata.R. In the latter I
+# selected female worms randomly and male worms accordingly not at
+# random (because of size and preparation issues the biggest worms
+# were used
+
+# The following lines just select what is handed in to the GenePool
+# for tag-sequencing
