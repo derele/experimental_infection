2861f26a24bc6316f088afe203bbb6d1e0a9b7db
derele <emanuelheitlinger@gmail.com>
2013-02-06 17:34:25 +0100

    folder renaming

---
+++ rename_folder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+### ran only once to produce renamed files
+
+# cd /data/RNAseq/renamed/;
+
+# for fol in $(ls -d ../raw_folders/A*_*) 
+# do
+#      cat $fol/*1.solfastq.gz.fastq > $(basename $fol)"_left.fastq"
+#      cat $fol/*2.solfastq.gz.fastq > $(basename $fol)"_right.fastq"
+# done
